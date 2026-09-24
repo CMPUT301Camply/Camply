31191508adbcf262fe89b2e21669f5ce36f11816
Filip Kosmajac <36778915+fkosmajac@users.noreply.github.com>
2026-09-23 19:17:47 -0600

    Update Shape.kt

--- app/src/main/java/com/example/camply/Shape.kt
+++ app/src/main/java/com/example/camply/Shape.kt
@@ -3,6 +3,6 @@ package com.example.camply
 abstract class Shape(x1: Int, y1: Int) {
     abstract val x : Int
     abstract val y : Int
-    val color: String = "pinkish"
+    val color: String = "red"
     
 }
